8a2714eb807fd88acc99bfa8b00a234dfd97aba5
KatMaldon <36998937+katmaldon@users.noreply.github.com>
2023-03-17 13:23:47 -0400

    background color landing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="app" class="bg-green-600 py-56 min-h-screen">
     <div class="container mx-auto text-center px-8">
-      <h2 class="text-3xl lg:text-6xl font-bold mb-2 text-white">Coming Soon</h2>
+      <h2 class="text-3xl lg:text-6xl font-semibold mb-2 text-white">Dreams of Persia</h2>
+      <h4 class="text-xl lg:text-2xl font-semibold mb-2 text-white">coming soon</h4>
     </div>
   </div>
 </template>
@@ -14,6 +15,7 @@ export default {
 
 <style>
 #app {
-  background-color: #112A96;
+    background: rgb(0,212,255);
+    background: radial-gradient(circle, rgba(0,212,255,1) 0%, rgba(17,42,150,1) 64%, rgba(2,0,36,1) 100%);
 }
 </style>
